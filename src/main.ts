0daff8336481a9860fa250e11a6d9eca71bd4147
kydner <mhd.rendika@gmail.com>
2021-04-08 11:02:38 +0700

    change option swagger

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
+  const options = new DocumentBuilder()
     .setTitle('Nestjs Base')
     .setDescription('Nestjs Base API description')
     .setVersion('1.0')
     .addTag('cats')
+    .addBearerAuth()
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
